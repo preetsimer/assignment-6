56a0030fa99214226663ffbb160b830c84cbd6f7
preetsimer <simerpreet1851.cse16@chitkara.edu.in>
2018-08-22 22:19:49 +0530

    assignment 6

---
+++ assignment-6.py
@@ -0,0 +1,46 @@
+#question 1
+def area(r):
+    a=4*r*r*3.14
+    print("The area of sphere is ",a)
+n=int(input("Enter radius of sphere: "))
+area(n)
+
+#question 2
+def perfect(num):
+    sum=0
+    for i in range(1,num):
+        if num%i==0:
+            sum=sum+i
+    if sum==num:
+        print("Perfect Number")
+    else:
+        print("Not a Perfect Number")
+n=int(input("Enter a number: "))
+perfect(n)
+
+
+#question 3
+n=int(input("Enter a number: "))
+print("The multiplication table of %d is:"%n)
+for i in range(1,11):
+    m=i*n
+    print("%d * %d = %d"%(n,i,m))
+
+#question 4
+def power(a,b):
+    if(b==1):
+        return(a)
+    if(b!=1):
+        return(a*power(a,b-1))
+a=int(input("Enter number: "))
+b=int(input("Enter exponential value: "))
+p=power(a,b)
+print("%d^%d=%d"%(a,b,p))
+
+
+
+
+
+
+
+    
